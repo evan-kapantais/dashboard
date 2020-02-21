6a51e7e4e2b7fa481708f6c40978da05f07bb271
evan-kapantais <evankapantais@gmail.com>
2020-02-21 15:15:05 +0100

    add geolocation in api call

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-//TODO: add forecast (check if possible with a single api call) 
+//TODO: add forecast (check if possible with a single api call)
+//TODO: optimize weather flow
+//TODO: imperial / metric toggle
 
 const WeatherWidget = styled.div `
   border: 1px solid;
@@ -25,12 +27,29 @@ export class Weather extends Component {
 
   abortController = new AbortController();
 
-  toCelcius = (temp) => (
-    temp - 273.15
-  );
+  weatherInit = () => {
 
-  getWeatherData = () => {
-    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=Barcelona,ES&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    const success = (position) => {
+      this.getWeatherData(position.coords.latitude, position.coords.longitude);
+    }
+
+    const error = () => {
+      alert('Unable to retreive location.');
+    }
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      alert('Your browser does not support location tracking, or permission is denied.');
+    }
+  }
+
+  getWeatherData = (lat, lon) => {
+
+    // const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    
+
     fetch(weatherApi, { signal: this.abortController.signal })
     .then(response => response.json())
     .then(
@@ -41,9 +60,9 @@ export class Weather extends Component {
             city: result.name,
             condition: result.weather[0].description,
             icon: result.weather[0].icon,
-            temp: Math.round(this.toCelcius(result.main.temp)),
-            tempMin: Math.round(this.toCelcius(result.main.temp_min)),
-            tempMax: Math.round(this.toCelcius(result.main.temp_max)),
+            temp: Math.round(result.main.temp),
+            tempMin: Math.round(result.main.temp_min),
+            tempMax: Math.round(result.main.temp_max),
             humidity: result.main.humidity,
             windSpeed: result.wind.speed,
             windDeg: result.wind.deg,
@@ -60,21 +79,24 @@ export class Weather extends Component {
   }
 
   componentDidMount = () => {
-    this.getWeatherData();
+    // this.getGeolocation();
+    this.weatherInit();
   }
 
   render() {
     return (
       <WeatherWidget>
         <header>
-          <img src={`http://openweathermap.org/img/wn/${this.state.weatherData.icon}@2x.png`} alt=""/>
+          <img src={`http://openweathermap.org/img/wn/${this.state.weatherData.icon}@2x.png`} alt='weather icon'/>
           <div>
-            <h1>{this.state.weatherData.city}</h1>
-            <p>{this.state.weatherData.condition}</p>
+            <h1>{this.state.weatherData.temp}°</h1>
+            <p>{this.state.weatherData.city}</p>
           </div>
         </header>
         <main>
+          <table>
 
+          </table>
         </main>
       </WeatherWidget>
     )
