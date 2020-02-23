f48f9dd540d735e17c138effc687bacc3937d6cc
evan-kapantais <evankapantais@gmail.com>
2020-02-23 20:26:18 +0100

    remove forecast flow

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import Days from './days'
-
-//TODO: add forecast (check if possible with a single api call)
-//TODO: optimize weather flow
 //TODO: imperial / metric toggle
 
 const WeatherWidget = styled.div `
@@ -21,8 +17,7 @@ export class Weather extends Component {
     super(props);
     this.state={
       isLoaded: false,
-      weatherCurrent: {},
-      weatherForecast: {},
+      weatherData: {},
     }
   }
 
@@ -47,36 +42,22 @@ export class Weather extends Component {
     }
   }
 
-  getForecastData = () => {
-    let currentDayIndex = new Date().getDay();
-    console.log(currentDayIndex);
-    const forecastDays = [];
-
-    for (let i = -0; i < 5; ++i) {
-      forecastDays.push(Days[(currentDayIndex + i) % Days.length]);
-    }
-    console.log(forecastDays);
-  }
-
   getWeatherData = (lat, lon) => {
-    const weatherApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
 
     fetch(weatherApi, { signal: this.abortController.signal })
     .then(response => response.json())
     .then(
       (result) => {
-        // console.log(result);
-        // console.log(result.list[0].dt_txt.slice(11, 13));
-        this.getForecastData();
         this.setState({
           isLoaded: true,
-          weatherCurrent: {
-            city: result.city.name,
-            condition: result.list[0].weather[0].description,
-            icon: result.list[0].weather[0].icon,
-            temp: Math.round(result.list[0].main.temp),
-            tempMin: Math.round(result.list[0].main.temp_min),
-            tempMax: Math.round(result.list[0].main.temp_max),
+          weatherData: {
+            city: result.name,
+            condition: result.weather[0].description,
+            icon: result.weather[0].icon,
+            temp: Math.round(result.main.temp),
+            tempMin: Math.round(result.main.temp_min),
+            tempMax: Math.round(result.main.temp_max),
           }
         });
       },
@@ -97,10 +78,10 @@ export class Weather extends Component {
     return (
       <WeatherWidget>
         <header>
-          <img src={`http://openweathermap.org/img/wn/${this.state.weatherCurrent.icon}@2x.png`} alt='weather icon'/>
+          <img src={`http://openweathermap.org/img/wn/${this.state.weatherData.icon}@2x.png`} alt='weather icon'/>
           <div>
-            <h1>{this.state.weatherCurrent.temp}°</h1>
-            <p>{this.state.weatherCurrent.city}</p>
+            <h1>{this.state.weatherData.temp}°</h1>
+            <p>{this.state.weatherData.city}</p>
           </div>
         </header>
         <main>
